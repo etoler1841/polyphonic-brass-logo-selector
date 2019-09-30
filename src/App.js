b8221bb62fc8c048efa02c7608e06fe86bce0ed9
Eric Toler <etoler1841@gmail.com>
2019-09-30 12:05:51 -0500

    Cleanup static files and assets

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Logo from './components/Logo'
 import FullName from './components/FullName'
 import Selector from './components/Selector'
 
+import colorOptions from './static/colors.json'
+import fontOptions from './static/fonts.json'
+
 function App() {
   const [selectedColor, setSelectedColor] = useState(1)
   const [selectedFont, setSelectedFont] = useState(2)
@@ -11,79 +14,23 @@ function App() {
   const [fontColor, setFontColor] = useState('#fff')
   const [fontFace, setFontFace] = useState('blackjack')
 
-  const colorOptions = [
-    {
-      id: 1,
-      text: 'White on Black',
-      bgColor: '#000',
-      fontColor: '#fff'
-    },
-    {
-      id: 2,
-      text: 'White on Medium Blue',
-      bgColor: '#0052a4',
-      fontColor: '#fff'
-    },
-    {
-      id: 3,
-      text: 'White on Medium Red',
-      bgColor: '#9c2020',
-      fontColor: '#fff'
-    },
-    {
-      id: 4,
-      text: 'White on Dark Blue',
-      bgColor: '#002e5d',
-      fontColor: '#fff'
-    },
-    {
-      id: 5,
-      text: 'White on Dark Green',
-      bgColor: '#224022',
-      fontColor: '#fff'
-    },
-    {
-      id: 6,
-      text: 'White on Dark Purple',
-      bgColor: '#402859',
-      fontColor: '#fff'
-    },
-    {
-      id: 7,
-      text: 'Light Blue on Medium Blue',
-      bgColor: '#0052a4',
-      fontColor: '#99ccff'
-    }
-  ]
-
-  const fontOptions = [
-    {
-      id: 1,
-      text: 'Milkshake',
-      fontFace: 'milkshake'
-    },
-    {
-      id: 2,
-      text: 'Black Jack',
-      fontFace: 'blackjack'
-    },
-    {
-      id: 3,
-      text: 'Fabfelt Script',
-      fontFace: 'fabfelt'
-    }
-  ]
+  useEffect(() => {
+    const color = Math.ceil(Math.random() * colorOptions.length)
+    const font = Math.ceil(Math.random() * fontOptions.length)
+    setSelectedColor(color)
+    setSelectedFont(font)
+  }, [])
 
   useEffect(() => {
     const _selected = colorOptions.find(opt => opt.id === selectedColor)
     setBgColor(_selected.bgColor)
     setFontColor(_selected.fontColor)
-  }, [selectedColor, colorOptions])
+  }, [selectedColor])
 
   useEffect(() => {
     const _selected = fontOptions.find(opt => opt.id === selectedFont)
     setFontFace(_selected.fontFace)
-  }, [selectedFont, fontOptions])
+  }, [selectedFont])
 
   return (
     <div
