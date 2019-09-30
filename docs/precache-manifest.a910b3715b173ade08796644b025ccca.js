ba1d7235d35c060869addc886639bcc18b1dfd57
Eric Toler <etoler1841@gmail.com>
2019-09-30 03:03:11 -0500

    Add a build to serve on Github Pages

---
+++ docs/precache-manifest.a910b3715b173ade08796644b025ccca.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248e3a0405988a3de12e5e46d508b0eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d9b5c1717e5cc781300",
+    "url": "/static/js/2.e145c6f7.chunk.js"
+  },
+  {
+    "revision": "b31ba1ede190fc1ff9ab",
+    "url": "/static/js/main.cb71224c.chunk.js"
+  },
+  {
+    "revision": "1960496df7ec9bb6f810",
+    "url": "/static/js/runtime-main.cd3826a1.js"
+  }
+]);
